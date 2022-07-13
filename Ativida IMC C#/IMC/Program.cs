595cc2a43ddab7b1be24329d0c5486b5ab15f08b
joaolucas404 <joaolucasssa21@gmail.com>
2022-07-13 17:37:37 -0300

    Calculadora de IMC C#

---
+++ Ativida IMC C#/IMC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double total, altura, peso; 
+            Console.WriteLine("Calcular IMC");
+            
+            Console.WriteLine("Insira o Peso");
+                peso = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira Altura");
+                altura = double.Parse(Console.ReadLine()); 
+            total = peso / (altura * altura);
+            Console.WriteLine("total " + total.ToString("F2"));
+            if (total < 18.5) 
+                Console.WriteLine("Magreza");
+                    if (total >= 18.5 && total < 24.9) 
+            Console.WriteLine("Normal");
+            if (total >= 24.9 && total < 30)
+            Console.WriteLine("Sobre Peso");
+            if (total >= 30) 
+            Console.WriteLine("Obesidade");
+
+
+        }
+    }
+}
