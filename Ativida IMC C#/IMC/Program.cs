768bbe617d85c1cdf733602ff59da1ffcd5d672d
joaolucas404 <joaolucasssa21@gmail.com>
2022-07-14 11:29:40 -0300

    Atividades C#

--- Ativida IMC C#/IMC/Program.cs
+++ Ativida IMC C#/IMC/Program.cs
@@ -6,18 +6,22 @@ namespace IMC
     {
         static void Main(string[] args)
         {
-            double total, altura, peso; 
+            string nome = ""; 
+            double total, altura, peso, idade; 
             Console.WriteLine("Calcular IMC");
-            
+            Console.WriteLine("Insira o seu Nome");
+            nome = Console.ReadLine();
+            Console.WriteLine("Insira sua Idade");
+                idade = double.Parse(Console.ReadLine());
             Console.WriteLine("Insira o Peso");
                 peso = double.Parse(Console.ReadLine());
             Console.WriteLine("Insira Altura");
                 altura = double.Parse(Console.ReadLine()); 
-            total = peso / (altura * altura);
+                total = peso / (altura * altura);
             Console.WriteLine("total " + total.ToString("F2"));
             if (total < 18.5) 
-                Console.WriteLine("Magreza");
-                    if (total >= 18.5 && total < 24.9) 
+            Console.WriteLine("Magreza");
+            if (total >= 18.5 && total < 24.9) 
             Console.WriteLine("Normal");
             if (total >= 24.9 && total < 30)
             Console.WriteLine("Sobre Peso");
